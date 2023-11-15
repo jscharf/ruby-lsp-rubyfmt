cb41acbe1066d1d6b18921655804752e402b72db
Joshua Scharf <joshua.scharf@fullscript.com>
2023-11-15 12:02:27 -0800

    Adjust implementation to use STDIN as input for source

--- lib/ruby_lsp/ruby-lsp-rubyfmt/addon.rb
+++ lib/ruby_lsp/ruby-lsp-rubyfmt/addon.rb
@@ -31,8 +31,7 @@ module RubyLsp
       # The main part of the interface is implementing the run method. It receives the URI and the document being formatted.
       # IMPORTANT: This method must return the formatted document source without mutating the original one in document
       def run(uri, document)
-        formatted_source = `rubyfmt #{uri.to_standardized_path}`
-        formatted_source
+        `echo "class MyClass; end" | rubyfmt`
       end
     end
   end
